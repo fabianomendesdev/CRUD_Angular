c351006bd9410efbc22f7898215af9ce809e7f1b
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-09-22 23:52:00 -0300

    Update class 544

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -1,3 +1,5 @@
+import { ProductService } from './../../events/product/product.service';
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductReadComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = []
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.read().subscribe(products => {
+      this.products = products
+      console.log(products)
+    })
   }
 
 }
